6251e298ac94f7a972c0d305d37b9795b16985ee
RRicardoLiraG <rrlirag.20@gmail.com>
2025-03-25 18:00:09 -0600

    Clase 20/03/2025

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,69 @@
 <?php
 
+use GuzzleHttp\Psr7\Request;
+use Illuminate\Http\Request as HttpRequest;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome', ['nombre' => 'Roberto']);
+})->name('home'); // nombre de la ruta
+
+// Route::get('/hola', function () {
+//     return route('hola');
+// });
+
+// Route::get('/suma', function () {
+//     $x = 20;
+//     $y = 30;
+//     $suma = $x + $y;
+//     return $suma;
+// });
+
+// rutas con parametros obligatorios
+
+Route::get('/suma/{x}/{y}', function ($x, $y) {
+    $suma = $x + $y;
+    return 'La suma es: ' . $suma;
 });
+
+// Route::get('nombre/{name}', function ($name) {
+//     return 'Mi nombre es: ' . $name;
+// });
+
+// rutas con parametros opcionales
+
+// Route::get('nombre/{name?}', function ($name = 'Invitado') {
+//     return 'Mi nombre es: ' . $name;
+// });
+
+// rutas con expresiones regulares
+
+// Route::get('nombre/{name?}', function ($name = 'Invitado') {
+//     return 'Mi nombre es: ' . $name;
+// })->where('name', '[a-zA-Z]+');
+
+
+// Route::get('/suma/{x}/{y}', function ($x, $y) {
+//     $suma = $x + $y;
+//     return 'La suma es: ' . $suma;
+// })->where(['x' => '[0-9]+', 'y' => '[0-9]+']);
+
+// Route::get('/verificar', function (Request $request) {
+//     if($request->route()->named('verificar')) {
+//         return 'Estás en la ruta verificar';
+//     } else {
+//         return 'No estás en la ruta verificar';
+//     }
+// })->name('verificar');
+
+// Route::prefix('admin')->group(function () {
+//     Route::get('/primero', function () {
+//         return 'Primero';
+//     })->name('admin.primero');
+//     Route::get('/segundo', function () {
+//         return 'Segundo';
+//     })->name('admin.segundo');
+// });
+
+
+
